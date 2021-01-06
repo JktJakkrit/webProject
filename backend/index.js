710b9b50996867abb0db938877ed1b00cbaac1d3
Doittikorn <aseaways1234@gmail.com>
2021-01-06 17:50:08 +0700

    start app backend main

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,12 @@
-console.log("test");
+const express = require('express')
+const app = express()
+const config = require('./configs/app');
+
+
+const server = app.listen( config.port, () => {
+    //let host = server.address().address
+    // @ts-ignore
+    let port = server.address().port 
+    //console.log(`Server is running at http://${host}:${port}`)
+    console.log(`Example app listening at  http://localhost:${port}`)
+    });
