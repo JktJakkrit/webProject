dd6e2fcb1a4234fe97878f730ab527a05dea4464
dodo <aseaways1234@gail.com>
2021-01-23 19:46:41 +0700

    add postman file .json

--- backend/configs/uploadpic.js
+++ backend/configs/uploadpic.js
@@ -25,3 +25,4 @@ const upload = multer({storage: setting_storage, fileFilter: imageFilter})
 module.exports = upload
 
 //  https://stackabuse.com/handling-file-uploads-in-node-js-with-expres-and-multer/
+//  https://medium.com/pnpsolution/%E0%B8%A7%E0%B8%B4%E0%B8%98%E0%B8%B5%E0%B8%81%E0%B8%B2%E0%B8%A3-upload-%E0%B9%81%E0%B8%A5%E0%B8%B0-resize-%E0%B8%A3%E0%B8%B9%E0%B8%9B%E0%B8%94%E0%B9%89%E0%B8%A7%E0%B8%A2-node-js-5ebb0f18f496
