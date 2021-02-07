e20da67968e1968a2b9139e22615fc080df389d0
dodo <aseaways1234@gail.com>
2021-02-07 13:41:57 +0700

    fix update all test

--- backend/services/air.service.js
+++ backend/services/air.service.js
@@ -1,60 +1,61 @@
 const db = require("../configs/database");
 const config = require("../configs/app");
 const path = require("path");
-
+const SqlString = require('sqlstring');
 const methods = {
-    findAll: function() {
+    findAll: function () {
         return new Promise((resolve, reject) => {
             // db.connect(function(err){ console.log(err)});
-            db.query("SELECT * FROM `manage-air`", function(error, result) {
+            db.query("SELECT * FROM `manage-air`", function (error, result) {
                 if (error) return reject(error);
                 return resolve(result);
             });
             // db.end();
         });
     },
-    findById: function(id) {
+    findById: function (id) {
         return new Promise((resolve, reject) => {
             let sql = "SELECT * FROM `manage-air` where `air_sys_id` = " + id;
             // db.connect();
-            db.query(sql, function(error, result) {
+            db.query(sql, function (error, result) {
                 if (error) return reject(error);
                 return resolve(result);
             });
             // db.end();
         });
     },
-    postItemAir: function(path_pic, body) {
+    postItemAir: function (path_pic, body) {
         return new Promise((resolve, reject) => {
             let newPath = config.file + path.basename(path_pic);
-            let pic = { file: newPath };
-            let data = {...body, ...pic };
+            let pic = {file: newPath};
+            let data = {...body, ...pic};
             let sql = "INSERT INTO `manage-air` SET ?";
             // return resolve(data);
-            db.query(sql, data, function(error, result) {
-                
+            db.query(sql, data, function (error, result) {
+
                 if (error) return reject(error);
                 return resolve(result);
             });
             // db.end();
         });
     }, // post
-    putItemAirById: function (path_pic,id, body) {
+    putItemAirById: function (path_pic, id, body) {
         return new Promise((resolve, reject) => {
             let newPath = config.file + path.basename(path_pic)
             let pic = {'file': newPath}
             let data = {...body, ...pic};
-            let sql = "UPDATE `manage-air` SET ? WHERE `air_sys_id` = ?";
-            db.query(sql, [data, id], function (error, result) {
+            let sql = SqlString.format('UPDATE `manage-air` SET ?', data);
+            sql = sql + "where `air_sys_id` = " + id ;
+            db.query(sql, data, function (error, result) {
                 if (error) return reject(error);
-                return resolve("update successfully");
+                return resolve(result);
             });
         });
     },
-    deleteItemAirById: function(id) {
+    deleteItemAirById: function (id) {
         return new Promise((resolve, reject) => {
             let sql = "DELETE FROM `manage-air` WHERE `air_sys_id` = " + id;
-            db.query(sql, function(error, result) {
+            db.query(sql, function (error, result) {
                 if (error) return reject(error);
                 return resolve("delete successfully");
             });
@@ -62,4 +63,4 @@ const methods = {
     },
 };
 
-module.exports = {...methods };
+module.exports = {...methods};
